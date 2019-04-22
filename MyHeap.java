fe6801742cce14692d702d03d075f6f51fbebb68
unknown <clee010922@gmail.com>
2019-04-21 21:04:56 -0400

    added pushDown method

--- MyHeap.java
+++ MyHeap.java
@@ -1,7 +1,24 @@
 public class MyHeap {
 
   private static void pushDown(int[] data, int size, int index) {
-
+    int left = 2*index+1;
+    int right = 2*index+2;
+    int max;
+    if (right == size) {
+      if (data[index] < data[left]) {
+        swap(data, index, left);
+        index = left;
+      }
+    }
+    else if (right < size) {
+      if (data[right] > data[left])
+        max = right;
+      else max = left;
+      if (data[index] < data[max]) {
+        swap(data, index, max);
+        index = max;
+      } 
+    }
   }
 
   private static void pushUp(int[] data, int index) {
