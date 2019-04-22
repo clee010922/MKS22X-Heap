fad2ad455b1047899b089393ad00d80eacc53b0c
unknown <clee010922@gmail.com>
2019-04-21 20:52:57 -0400

    added swap method

---
+++ MyHeap.java
@@ -0,0 +1,25 @@
+public class MyHeap {
+
+  private static void pushDown(int[] data, int size, int index) {
+
+  }
+
+  private static void pushUp(int[] data, int index) {
+
+  }
+
+  public static void heapify(int[] data) {
+
+  }
+
+  public static void heapsort(int[] data) {
+
+  }
+
+  public static void swap(int[] data, int a, int b) {
+    int temp = data[a];
+    data[a] = data[b];
+    data[b] = temp;
+  }
+
+}
